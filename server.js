b4c5e388f316e1bd7c5fa6e6da6ee2f4a369a7f4
p-prince-n <pp2358296@gmail.com>
2025-08-13 00:37:31 +0530

    updating some controller

--- server.js
+++ server.js
@@ -38,4 +38,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server listening on port http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
